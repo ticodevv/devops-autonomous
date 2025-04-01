5d5e81f3e92d5796d866cd3b2ec6ebd832fbafec
0x00 0x00 <0x000x00@Mac.lan>
2025-04-01 12:03:49 +0200

    pascal

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">=1.3.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.47.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "38f1f9f0-f272-436a-af88-4d95d39cbe30"
+}
